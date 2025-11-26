10569e072e25d015a0cb8189876e35e34411ea0e
Pooja <poojaverma97491@gmail.com>
2025-11-26 17:40:08 +0530

    vercel.json

--- server/FirebaseAdmin.ts
+++ server/FirebaseAdmin.ts
@@ -1,24 +1,13 @@
-// import admin from "firebase-admin";
-// import "dotenv/config";
-
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.FIREBASE_PROJECT_ID,
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-//     }),
-//   });
-// }
-
-// export { admin };
 import admin from "firebase-admin";
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+import "dotenv/config";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
